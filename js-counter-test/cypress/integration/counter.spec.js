97c353b10af5aaee6c6d21bf105490c8337bb37e
kimtaenam <cjb940103@gmail.com>
2022-03-20 01:38:42 +0900

    counter first test code 작성

--- js-counter-test/cypress/integration/counter.spec.js
+++ js-counter-test/cypress/integration/counter.spec.js
@@ -1,11 +1,9 @@
-// describe('My First Test', () => {
-//   it('Does not do much!', () => {
-//     expect(true).to.equal(true)
-//   })
-// })
+describe("example counter app", () => {
+  beforeEach(() => {
+    cy.visit("http://127.0.0.1:5500/js-counter-test/index.html");
+  });
 
-describe("My First Test", () => {
-  it("Does not do much!", () => {
-    expect(true).to.equal(true);
+  it('display initial value 0', () => {
+      cy.get('#value').invoke("text").should('eq', "1");
   });
 });
