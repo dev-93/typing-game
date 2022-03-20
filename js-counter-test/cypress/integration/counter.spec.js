9e6e6c680ccf90842eb01b924d38a9682e2c1f4f
kimtaenam <cjb940103@gmail.com>
2022-03-20 17:07:27 +0900

    카운터 +, -기능 테스트 작성

--- js-counter-test/cypress/integration/counter.spec.js
+++ js-counter-test/cypress/integration/counter.spec.js
@@ -3,7 +3,36 @@ describe("example counter app", () => {
     cy.visit("http://127.0.0.1:5500/index.html");
   });
 
-  it("display initial value 0", () => {
+  it("counter의 초기값은 0이다.", () => {
     cy.get("#value").invoke("text").should("eq", "0");
+  });
+
+  it("+ 버튼을 클릭 시 count가 1증가한다.", () => {
+    // 기존 값가져오고, + 버튼 클릭시 값이 +1인지 체크
+    cy.get("#value")
+      .invoke("text")
+      .then((value) => {
+        const preValue = Number(value);
+
+        cy.get(".increase-btn").click();
+        cy.get("#value")
+          .invoke("text")
+          .should("eq", String(preValue + 1));
+      });
+  });
+
+  it("- 버튼을 클릭 시 count가 1감소한다.", () => {
+    // + 버튼 클릭해서 값 1로 만들고, - 버튼 클릭시 값이 0인지 체크
+    cy.get(".increase-btn").click();
+    cy.get("#value")
+      .invoke("text")
+      .then((value) => {
+        const preValue = Number(value);
+
+        cy.get(".decrease-btn").click();
+        cy.get("#value")
+          .invoke("text")
+          .should("eq", String(preValue - 1));
+      });
   });
 });
