02ba62a4d3f1dfaf1be3c73f29ab5ba2e300c8f0
kimtaenam <cjb940103@gmail.com>
2022-03-20 16:55:08 +0900

    app 수정

--- js-counter-test/cypress/integration/counter.spec.js
+++ js-counter-test/cypress/integration/counter.spec.js
@@ -1,9 +1,9 @@
 describe("example counter app", () => {
   beforeEach(() => {
-    cy.visit("http://127.0.0.1:5500/js-counter-test/index.html");
+    cy.visit("http://127.0.0.1:5500/index.html");
   });
 
-  it('display initial value 0', () => {
-      cy.get('#value').invoke("text").should('eq', "1");
+  it("display initial value 0", () => {
+    cy.get("#value").invoke("text").should("eq", "0");
   });
 });
