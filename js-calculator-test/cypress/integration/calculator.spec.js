bf92e8cdf829204fc615542004bad6444a4bd422
kimtaenam <cjb940103@gmail.com>
2022-03-21 16:30:29 +0900

    예외사항 테스트

--- js-calculator-test/cypress/integration/calculator.spec.js
+++ js-calculator-test/cypress/integration/calculator.spec.js
@@ -54,4 +54,19 @@ describe("계산기 어플리케이션 테스트", () => {
     cy.get(".modifier").contains("AC").click();
     cy.get("#total").invoke("text").should("eq", "0");
   });
+
+  it("숫자는 한번에 최대 3자리 수까지 입력 가능하다", () => {
+    for (let i = 0; i < 5; i++) {
+      cy.get(".digit").contains("5").click();
+    }
+    cy.get("#total").invoke("text").should("eq", "555");
+  });
+
+  it("계산 결과를 표현할 때 소수점 이하는 버림", () => {
+    cy.get(".digit").contains("5").click();
+    cy.get(".operation").contains("/").click();
+    cy.get(".digit").contains("2").click();
+    cy.get(".operation").contains("=").click();
+    cy.get("#total").invoke("text").should("eq", "2");
+  });
 });
